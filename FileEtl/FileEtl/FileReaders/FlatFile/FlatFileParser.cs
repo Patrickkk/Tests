521e927c50c4c2eb0e16638850ab0614cc203e02
plo01 <plo01@DESKTOP-AFPP1C7>
2017-06-29 16:02:23 +0200

    started with flat file reader

---
+++ FileEtl/FileEtl/FileReaders/FlatFile/FlatFileParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FileEtl.FileReaders.FlatFile
+{
+    internal class FlatFileParser : IDisposable
+    {
+        private object p;
+
+        public FlatFileParser(object p)
+        {
+            this.p = p;
+        }
+
+        #region IDisposable Support
+
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+
+        #endregion IDisposable Support
+    }
+}
--- FileEtl/FileEtl/FileReaders/FlatFile/FlatFileReader.cs
+++ FileEtl/FileEtl/FileReaders/FlatFile/FlatFileReader.cs
@@ -5,9 +5,28 @@ namespace FileEtl.FileReaders.FlatFile
 {
     public class FlatFileReader
     {
-        public DataSet ReadOrderFile(FileInfo input, FlatFileReaderConfiguration configuration)
+        public DataSet ReadOrderFile(FileInfo file, FlatFileReaderConfiguration configuration)
         {
             var dataset = DataTableCreator.CreateDataSetForConfig(configuration);
+
+            using (var parser = new FlatFileParser(file.OpenText()))
+            {
+                while (true)
+                {
+                    var row = parser.Read();
+                    if (row == null)
+                    {
+                        break;
+                    }
+                    var recordType = recordSelector.SelectRecordForRow(config.CsvRecords, config, row);
+                    var table = result.Tables[recordType.TableName];
+                    var newRow = table.NewRow();
+                    CsvFieldMapper.Mapfields(row, recordType, newRow);
+                    table.Rows.Add(newRow);
+                }
+            }
+            return result;
+
             return dataset;
         }
     }
