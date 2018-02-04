88e3ef3d2424d962738d4c7090b874aefe9a98b4
plo01 <plo01@DESKTOP-AFPP1C7>
2018-02-04 07:43:32 +0100

    started more advanced solving

--- SudokuSolver/FieldValue.cs
+++ SudokuSolver/FieldValue.cs
@@ -7,9 +7,8 @@ namespace SudokuSolver
 {
     public class FieldValue
     {
-        public static FieldValue FieldValueDefault { get { return new FieldValue { PossibleValues = new PossibleValues() }; } }
         public int KnownValue { get; set; } = 0;
-        public PossibleValues PossibleValues { get; set; } = null;
+        public List<int> PossibleValues { get; set; } = null;
         public int X { get; set; }
 
         public int Y { get; set; }
@@ -17,7 +16,7 @@ namespace SudokuSolver
         public void SetKnownValue(int value)
         {
             this.KnownValue = value;
-            this.PossibleValues = null;
+            this.PossibleValues = new List<int> { value };
         }
 
         //private void Match(Action<int> knownValue, Action<PossibleValues> possibleValues)
@@ -47,9 +46,19 @@ namespace SudokuSolver
             {
                 for (int y = 1; y < 10; y++)
                 {
-                    values.Add(new FieldValue { X = x, Y = y, PossibleValues = new PossibleValues() });
+                    values.Add(new FieldValue { X = x, Y = y, PossibleValues = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 } });
                 }
             }
+        }
+
+        public static IEnumerable<FieldValue> FieldValuesInBlock(PlayingField playingField, int x, int y, int blockXOffset, int blockYOffset)
+        {
+            var xdiv = (x + blockXOffset - 1) / 3;
+            var ydiv = (y + blockYOffset - 1) / 3;
+            return playingField.values.Where(v =>
+                (v.X - 1) / 3 == xdiv &&
+                (v.Y - 1) / 3 == ydiv
+            );
         }
 
         public static IEnumerable<int> PossibleValuesAtPosition(PlayingField field, int x, int y)
@@ -63,12 +72,14 @@ namespace SudokuSolver
             return possibleValues
                 .Except(ValuesInRow(field, y))
                 .Except(ValuesInCollum(field, x))
-                .Except(ValuesInBlock(field, x, y));
+                .Except(ValuesInBlock(field, x, y))
+                .Except(ValuesThatMustBeInRow(field, x, y))
+                .Except(ValuesThatMustBeInCollum(field, x, y));
         }
 
         public static PlayingField Solve(PlayingField puzzle)
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 for (int x = 1; x < 10; x++)
                 {
@@ -77,6 +88,10 @@ namespace SudokuSolver
                         if (puzzle.FieldValue(x, y).KnownValue == 0)
                         {
                             var possibleValues = PlayingField.PossibleValuesAtPosition(puzzle, x, y);
+                            if (possibleValues.Count() == 0)
+                            {
+                                throw new Exception("Zero options left");
+                            }
                             if (possibleValues.Count() == 1)
                             {
                                 puzzle.FieldValue(x, y).SetKnownValue(possibleValues.ElementAt(0));
@@ -84,7 +99,7 @@ namespace SudokuSolver
                         }
                     }
                 }
-                if (!puzzle.values.Any(x => x.KnownValue > 0))
+                if (NoRemainingUnknownValues(puzzle))
                 {
                     return puzzle;
                 }
@@ -100,12 +115,7 @@ namespace SudokuSolver
 
         public static IEnumerable<int> ValuesInBlock(PlayingField playingField, int x, int y, int blockXOffset, int blockYOffset)
         {
-            var xdiv = (x + blockXOffset - 1) / 3;
-            var ydiv = (y + blockYOffset - 1) / 3;
-            return playingField.values.Where(v =>
-                (v.X - 1) / 3 == xdiv &&
-                (v.Y - 1) / 3 == ydiv
-            ).Select(f => f.KnownValue);
+            return FieldValuesInBlock(playingField, x, y, blockXOffset, blockYOffset).Select(f => f.KnownValue);
         }
 
         public static IEnumerable<int> ValuesInCollum(PlayingField playingField, int x)
@@ -116,6 +126,46 @@ namespace SudokuSolver
         public static IEnumerable<int> ValuesInRow(PlayingField playingField, int y)
         {
             return playingField.values.Where(v => v.Y == y && v.KnownValue > 0).Select(f => f.KnownValue);
+        }
+
+        public static IEnumerable<int> ValuesThatMustBeInCollum(PlayingField playingField, int x, int y)
+        {
+            if (y <= 3)
+            {
+                return ValuesThatMustBeInCollumBlock(playingField, x, 2)
+                    .Concat(ValuesThatMustBeInCollumBlock(playingField, x, 3));
+            }
+            if (y > 3 && x <= 6)
+            {
+                return ValuesThatMustBeInCollumBlock(playingField, x, 1)
+                     .Concat(ValuesThatMustBeInCollumBlock(playingField, x, 3));
+            }
+            if (y > 6)
+            {
+                return ValuesThatMustBeInCollumBlock(playingField, x, 1)
+                    .Concat(ValuesThatMustBeInCollumBlock(playingField, x, 2));
+            }
+            throw new Exception(":(");
+        }
+
+        public static IEnumerable<int> ValuesThatMustBeInRow(PlayingField playingField, int x, int y)
+        {
+            if (x <= 3)
+            {
+                return ValuesThatMustBeInRowBlock(playingField, y, 2)
+                    .Concat(ValuesThatMustBeInRowBlock(playingField, y, 3));
+            }
+            if (x > 3 && x <= 6)
+            {
+                return ValuesThatMustBeInRowBlock(playingField, y, 1)
+                     .Concat(ValuesThatMustBeInRowBlock(playingField, y, 3));
+            }
+            if (x > 6)
+            {
+                return ValuesThatMustBeInRowBlock(playingField, y, 1)
+                    .Concat(ValuesThatMustBeInRowBlock(playingField, y, 2));
+            }
+            throw new Exception(":(");
         }
 
         public FieldValue FieldValue(int x, int y)
@@ -136,12 +186,26 @@ namespace SudokuSolver
             }
             return builder.ToString();
         }
-    }
 
-    public class PossibleValues : List<int>
-    {
-        public PossibleValues() : base(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 })
+        private static bool NoRemainingUnknownValues(PlayingField puzzle)
         {
+            return !puzzle.values.Any(x => x.KnownValue == 0);
+        }
+
+        private static IEnumerable<int> ValuesThatMustBeInCollumBlock(PlayingField playingField, int x, int block)
+        {
+            var valuesInFirstBlock = FieldValuesInBlock(playingField, block * 3, x, 0, 0);
+            var valuesInSameX = valuesInFirstBlock.Where(field => field.X == x).SelectMany(f => f.PossibleValues);
+            var valuesNotInSameX = valuesInFirstBlock.Where(field => field.X != x).SelectMany(f => f.PossibleValues);
+            return valuesInSameX.Except(valuesNotInSameX);
+        }
+
+        private static IEnumerable<int> ValuesThatMustBeInRowBlock(PlayingField playingField, int y, int block)
+        {
+            var valuesInFirstBlock = FieldValuesInBlock(playingField, block * 3, y, 0, 0);
+            var valuesInSameY = valuesInFirstBlock.Where(field => field.Y == y).SelectMany(x => x.PossibleValues);
+            var valuesNotInSameY = valuesInFirstBlock.Where(field => field.Y != y).SelectMany(x => x.PossibleValues);
+            return valuesInSameY.Except(valuesNotInSameY);
         }
     }
 }
