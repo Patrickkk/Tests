b16770d40491900becfe59e2862caec342f10c43
plo01 <plo01@DESKTOP-AFPP1C7>
2018-03-09 09:49:22 +0100

    sync

--- Angular-Extended-Grid/Extended.material.grid/src/app/app.module.ts
+++ Angular-Extended-Grid/Extended.material.grid/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
