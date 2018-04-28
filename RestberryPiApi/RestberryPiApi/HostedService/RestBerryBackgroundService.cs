33d80730ebe1d0d32a38c4105f19a02d85956e25
Patrickkk <patrickvanlohuizen@outlook.com>
2018-04-28 08:33:02 +0200

    dont throw exception

--- RestberryPiApi/RestberryPiApi/HostedService/RestBerryBackgroundService.cs
+++ RestberryPiApi/RestberryPiApi/HostedService/RestBerryBackgroundService.cs
@@ -11,12 +11,12 @@ namespace RestberryPiApi.HostedService
     {
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
     }
 }
